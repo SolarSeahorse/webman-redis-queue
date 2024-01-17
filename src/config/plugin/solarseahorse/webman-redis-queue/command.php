ad7fdaf3b47c46a542a814d037deb49299611640
SolarSeahorse <unique666@duck.com>
2024-01-18 01:12:38 +0800

    Add ConsumerListCommand to command config

--- src/config/plugin/solarseahorse/webman-redis-queue/command.php
+++ src/config/plugin/solarseahorse/webman-redis-queue/command.php
@@ -2,5 +2,6 @@
 return [
     SolarSeahorse\WebmanRedisQueue\Commands\MakeConsumerCommand::class,
     SolarSeahorse\WebmanRedisQueue\Commands\RemoveConsumerCommand::class,
-    SolarSeahorse\WebmanRedisQueue\Commands\CleanRedisDataCommand::class
+    SolarSeahorse\WebmanRedisQueue\Commands\CleanRedisDataCommand::class,
+    SolarSeahorse\WebmanRedisQueue\Commands\ConsumerListCommand::class
 ];
